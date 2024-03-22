e631dbf32a961d97d1e71e0f5aa70c239fc24cda
Jair Palma <jpalma.teachme@gmail.com>
2024-03-21 18:49:15 -0600

    added pages

--- StylingResourcesMAUI/App.xaml.cs
+++ StylingResourcesMAUI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace StylingResourcesMAUI;
+﻿using NavigationPatternsMAUI.Navigations;
+
+namespace StylingResourcesMAUI;
 
 public partial class App : Application
 {
@@ -6,7 +8,7 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		MainPage = new MyTabbedPage();
 	}
 }
 
